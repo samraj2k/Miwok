d45160df2a50715e713eac61a6992ac042a4e4c3
amangrobo <amangrobo@gmail.com>
2018-12-01 13:10:26 +0530

    Added Image functions

--- app/src/main/java/com/grobo/miwok1/Colors.java
+++ app/src/main/java/com/grobo/miwok1/Colors.java
@@ -14,16 +14,14 @@ public class Colors extends AppCompatActivity {
         setContentView(R.layout.word_list);
 
         ArrayList<Word> words = new ArrayList<Word>();
-        words.add(new Word("one", "lutti"));
-        words.add(new Word("two", "otiiko"));
-        words.add(new Word("three", "tolookosu"));
-        words.add(new Word("four", "oyyisa"));
-        words.add(new Word("five", "massokka"));
-        words.add(new Word("six", "temmokka"));
-        words.add(new Word("seven", "kenekaku"));
-        words.add(new Word("eight", "kawinta"));
-        words.add(new Word("nine", "wo'e"));
-        words.add(new Word("ten", "na'aacha"));
+        words.add(new Word("one", "lutti", R.drawable.color_red));
+        words.add(new Word("two", "otiiko", R.drawable.color_green));
+        words.add(new Word("three", "tolookosu", R.drawable.color_brown));
+        words.add(new Word("four", "oyyisa", R.drawable.color_gray));
+        words.add(new Word("five", "massokka", R.drawable.color_black));
+        words.add(new Word("six", "temmokka", R.drawable.color_white));
+        words.add(new Word("seven", "kenekaku", R.drawable.color_dusty_yellow));
+        words.add(new Word("eight", "kawinta", R.drawable.color_mustard_yellow));
 
         ListView numbersListView = (ListView) findViewById(R.id.numbersListView);
         WordAdapter adapter = new WordAdapter(this, words);
--- app/src/main/java/com/grobo/miwok1/FamilyMembers.java
+++ app/src/main/java/com/grobo/miwok1/FamilyMembers.java
@@ -14,16 +14,16 @@ public class FamilyMembers extends AppCompatActivity {
         setContentView(R.layout.word_list);
 
         ArrayList<Word> words = new ArrayList<Word>();
-        words.add(new Word("one", "lutti"));
-        words.add(new Word("two", "otiiko"));
-        words.add(new Word("three", "tolookosu"));
-        words.add(new Word("four", "oyyisa"));
-        words.add(new Word("five", "massokka"));
-        words.add(new Word("six", "temmokka"));
-        words.add(new Word("seven", "kenekaku"));
-        words.add(new Word("eight", "kawinta"));
-        words.add(new Word("nine", "wo'e"));
-        words.add(new Word("ten", "na'aacha"));
+        words.add(new Word("one", "lutti", R.drawable.family_father));
+        words.add(new Word("two", "otiiko", R.drawable.family_mother));
+        words.add(new Word("three", "tolookosu", R.drawable.family_son));
+        words.add(new Word("four", "oyyisa", R.drawable.family_daughter));
+        words.add(new Word("five", "massokka", R.drawable.family_older_brother));
+        words.add(new Word("six", "temmokka", R.drawable.family_younger_brother));
+        words.add(new Word("seven", "kenekaku", R.drawable.family_older_sister));
+        words.add(new Word("eight", "kawinta", R.drawable.family_younger_sister));
+        words.add(new Word("nine", "wo'e", R.drawable.family_grandfather));
+        words.add(new Word("ten", "na'aacha", R.drawable.family_grandmother));
 
         ListView numbersListView = (ListView) findViewById(R.id.numbersListView);
         WordAdapter adapter = new WordAdapter(this, words);
